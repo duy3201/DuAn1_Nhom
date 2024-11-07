b0901dae0f1c05cb975f95aeafae9f1a3e11b5a4
nguyenvanla <nguyenvanla2002@gmail.com>
2024-11-07 18:51:08 +0700

    [client]-header,footer

--- App/Controllers/Client/HomeController.php
+++ App/Controllers/Client/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Client;
 
 use App\Helpers\NotificationHelper;
 use App\Views\Client\Components\Notification;
+use App\Views\Client\Layouts\Footer;
 use App\Views\Client\Layouts\Header;
 
 class HomeController
@@ -12,5 +13,6 @@ class HomeController
     public static function index()
     {
         Header::render();
+        Footer::render();
     }
 }
---
+++ App/Views/Client/Layouts/Footer.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Views\Client\Layouts;
+
+use App\Views\BaseView;
+
+class Footer extends BaseView
+{
+
+     public static function render($data = null)
+     { ?>
+          <footer>
+            <div class="footer1">
+                Logo
+                <p>Nâng tầm phong cách với quần áo chất lượng, giá hợp lý, và mỗi món đồ đều mang một câu chuyện riêng biệt.</p>
+            </div>
+            <hr>
+            <div class="container-footer text-center footer2">
+                <div class="row">
+                    <div class="col">
+                        <h4>Về chúng tôi</h4>
+                        <nav></nav>
+                        <a href="#">Tổng quan cửa hàng</a>
+                        <a href="#">Tìm kiếm sản phẩm</a>
+                        <a href="#">Lịch sử cửa hàng</a>
+                        <a href="#">Tín tức mới nhất</a>
+                    </div>
+                    <div class="col">
+                        <h4>Chính sách</h4>
+                        <nav></nav>
+                        <a href="#">Chính sách đổi trả</a>
+                        <a href="#">Chính sách khuyến mãi</a>
+                        <a href="#">Chính sách bảo mật</a>
+                        <a href="#">Chính sách giao hàng</a>
+                    </div>
+                    <div class="col">
+                        <h4>Liên hệ</h4>
+                        <nav></nav>
+                        <a href="#">: nguyenvanla@gmail.com</a>
+                        <a href="#">: 0329396779</a>
+                        <a href="#">: Đường số 22, Thường Thạnh, Cái Răng, Cần Thơ</a>
+                    </div>
+                    <div class="col">
+                        <h4>Liên kết</h4>
+                        <nav></nav>
+                        <div class="icon-links">
+                            <a href="#"><img src="/public/img/fb.png" alt=""></a>
+                            <a href="#"><img src="/public/img/tt.png" alt=""></a>
+                            <a href="#"><img src="/public/img/insta.png" alt=""></a>
+                            <a href="#"><img src="/public/img/youtube.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="container-footer3 ">
+                <div class="row justify-content-between">
+                    <div class="col-4">
+                        Thiết kế bởi team Old Style
+                    </div>
+                    <div class="col-4">
+                        <a href="#"><img src="/public/img/Visa.png" alt=""></a>
+                        <a href="#"><img src="/public/img/Mastercard.png" alt=""></a>
+                        <a href="#"><img src="/public/img/GooglePay.png" alt=""></a>
+                        <a href="#"><img src="/public/img/PayPal.png" alt=""></a>
+                        <a href="#"><img src="/public/img/ApplePay.png" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </footer>
+<?php }
+}
