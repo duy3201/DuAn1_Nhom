987677ce1797c186e514b4dcd00babb9ca09338f
duy3201 <154608848+duy3201@users.noreply.github.com>
2024-11-07 11:13:41 +0700

    [Main]-Source

--- index.php
+++ index.php
@@ -10,44 +10,43 @@ use App\Route;
 
 require_once 'vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 require_once 'config.php';
 
 
-require_once 'App/Views/index.php';
+
+// *** Client
+Route::get('/', 'App\Controllers\Client\HomeController@index');
+Route::get('/products', 'App\Controllers\Client\ProductController@index');
+Route::get('/products/{id}', 'App\Controllers\Client\ProductController@detail');
 
 
+// *** Admin
 
-// // *** Client
-// Route::get('/', 'App\Controllers\Client\Header@index');
-// Route::get('/products', 'App\Controllers\Client\ProductController@index');
-// Route::get('/products/{id}', 'App\Controllers\Client\ProductController@detail');
+Route::get('/admin', 'App\Controllers\Admin\HomeController@index');
 
+//  *** Category
+// GET /categories (lấy danh sách loại sản phẩm)
+Route::get('/admin/categories', 'App\Controllers\Admin\CategoryController@index');
 
-// // *** Admin
+// GET /categories/create (hiển thị form thêm loại sản phẩm)
+Route::get('/admin/categories/create', 'App\Controllers\Admin\CategoryController@create');
 
-// Route::get('/admin', 'App\Controllers\Admin\HomeController@index');
+// POST /categories (tạo mới một loại sản phẩm)
+Route::post('/admin/categories', 'App\Controllers\Admin\CategoryController@store');
 
-// //  *** Category
-// // GET /categories (lấy danh sách loại sản phẩm)
-// Route::get('/admin/categories', 'App\Controllers\Admin\CategoryController@index');
+// GET /categories/{id} (lấy chi tiết loại sản phẩm với id cụ thể)
+Route::get('/admin/categories/{id}', 'App\Controllers\Admin\CategoryController@edit');
 
-// // GET /categories/create (hiển thị form thêm loại sản phẩm)
-// Route::get('/admin/categories/create', 'App\Controllers\Admin\CategoryController@create');
+// PUT /categories/{id} (update loại sản phẩm với id cụ thể)
+Route::put('/admin/categories/{id}', 'App\Controllers\Admin\CategoryController@update');
 
-// // POST /categories (tạo mới một loại sản phẩm)
-// Route::post('/admin/categories', 'App\Controllers\Admin\CategoryController@store');
-
-// // GET /categories/{id} (lấy chi tiết loại sản phẩm với id cụ thể)
-// Route::get('/admin/categories/{id}', 'App\Controllers\Admin\CategoryController@edit');
-
-// // PUT /categories/{id} (update loại sản phẩm với id cụ thể)
-// Route::put('/admin/categories/{id}', 'App\Controllers\Admin\CategoryController@update');
-
-// // DELETE /categories/{id} (delete loại sản phẩm với id cụ thể)
-// Route::delete('/admin/categories/{id}', 'App\Controllers\Admin\CategoryController@delete');
+// DELETE /categories/{id} (delete loại sản phẩm với id cụ thể)
+Route::delete('/admin/categories/{id}', 'App\Controllers\Admin\CategoryController@delete');
 
 
 
 
-// Route::dispatch($_SERVER['REQUEST_URI']);
+Route::dispatch($_SERVER['REQUEST_URI']);
